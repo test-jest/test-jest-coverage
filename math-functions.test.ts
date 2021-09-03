83734c5237e9c2470e811d128f961ba389c7e5f6
Mughees Asif <mughees460@gmail.com>
2021-09-03 14:55:11 +0100

    more tests

--- math-functions.test.ts
+++ math-functions.test.ts
@@ -1,4 +1,4 @@
-import { sum, minus } from "./math-functions";
+import { sum, minus, multiply } from "./math-functions";
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
@@ -6,4 +6,8 @@ test('adds 1 + 2 to equal 3', () => {
 
 test('subtracts 1 - 2 to equal -1', () => {
   expect(minus(3, 1)).toBe(2);
+});
+
+test('multiplies 2 - 2 to equal 4', () => {
+  expect(multiply(2, 2)).toBe(4);
 });
