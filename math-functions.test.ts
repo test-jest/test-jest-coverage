8b4de1cc10a9767f408985d9e9ee85e02742988a
Federico Bozzini <federico.bozzini@arm.com>
2021-09-03 13:28:40 +0100

    Added minus test

--- math-functions.test.ts
+++ math-functions.test.ts
@@ -3,3 +3,7 @@ import { sum } from "./math-functions";
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('subtracts 1 - 2 to equal -1', () => {
+  expect(minus(1, 2)).toBe(-1);
+});
