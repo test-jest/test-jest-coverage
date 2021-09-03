5f3c09ef5d1dbe46231a68f2b439533b5a406303
Mughees Asif <mughees460@gmail.com>
2021-09-03 15:53:19 +0100

    test

--- math-functions.test.ts
+++ math-functions.test.ts
@@ -8,6 +8,6 @@ test('subtracts 1 - 2 to equal -1', () => {
   expect(minus(1, 2)).toBe(-1);
 });
 
-test('divide 20 / 5 to equal 2', () => {
+test('divide 20/5 to equal 2', () => {
   expect(divide(20, 5)).toBe(4);
 });
