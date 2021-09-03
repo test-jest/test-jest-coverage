0262e7f4d9b7efbaaef96546f2366d7c9a871001
Mughees Asif <mughees460@gmail.com>
2021-09-03 14:45:04 +0100

    failing tests

--- math-functions.test.ts
+++ math-functions.test.ts
@@ -1,9 +1,9 @@
-import { sum } from "./math-functions";
+import { sum, minus } from "./math-functions";
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
 test('subtracts 1 - 2 to equal -1', () => {
-  expect(minus(1, 2)).toBe(-1);
+  expect(minus(1, 2)).toBe(1);
 });
